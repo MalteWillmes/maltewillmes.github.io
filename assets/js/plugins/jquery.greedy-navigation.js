80423c74d9d8b54cf34bbe784a6132b717bcf0b9
Malte Willmes <malte.willmes@gmail.com>
2026-09-02 14:53:43 +0200

    Updated JS

--- assets/js/plugins/jquery.greedy-navigation.js
+++ assets/js/plugins/jquery.greedy-navigation.js
@@ -1,86 +1,451 @@
 /*
-* Greedy Navigation
-*
-* http://codepen.io/lukejacksonn/pen/PwmwWV
-*
-*/
+ * Greedy Navigation
+ *
+ * Modified so that on mobile (<= 768 px)
+ * ALL navigation links except persistent items
+ * collapse into the hamburger menu.
+ */
 
-var $nav = $('#site-nav');
-var $btn = $('#site-nav button');
-var $vlinks = $('#site-nav .visible-links');
-var $vlinks_persist_tail = $vlinks.children("*.persist.tail");
-var $hlinks = $('#site-nav .hidden-links');
+var $nav = $("#site-nav");
+var $btn = $("#site-nav button");
+var $vlinks = $("#site-nav .visible-links");
+var $hlinks = $("#site-nav .hidden-links");
+
+var $vlinksPersistTail =
+  $vlinks.children(".persist.tail");
 
 var breaks = [];
 
-function updateNav() {
+var mobileBreakpoint = 768;
+var mobileMode = false;
 
-  var availableSpace = $btn.hasClass('hidden') ? $nav.width() : $nav.width() - $btn.width() - 30;
 
-  // The visible list is overflowing the nav
-  if ($vlinks.width() > availableSpace) {
+/* =========================================
+   MOVE ONE HIDDEN ITEM BACK TO VISIBLE
+   ========================================= */
 
-    while ($vlinks.width() > availableSpace && $vlinks.children("*:not(.persist)").length > 0) {
-      // Record the width of the list
-      breaks.push($vlinks.width());
+function moveHiddenItemToVisible() {
 
-      // Move item to the hidden list
-      $vlinks.children("*:not(.persist)").last().prependTo($hlinks);
+  var $item =
+    $hlinks.children().first();
 
-      availableSpace = $btn.hasClass("hidden") ? $nav.width() : $nav.width() - $btn.width() - 30;
 
-      // Show the dropdown btn
-      $btn.removeClass("hidden");
-    }
-
-    // The visible list is not overflowing
-  } else {
-
-    // There is space for another item in the nav
-    while (breaks.length > 0 && availableSpace > breaks[breaks.length - 1]) {
-      // Move the item to the visible list
-      if ($vlinks_persist_tail.children().length > 0) {
-        $hlinks.children().first().insertBefore($vlinks_persist_tail);
-      } else {
-        $hlinks.children().first().appendTo($vlinks);
-      }
-      breaks.pop();
-    }
-
-    // Hide the dropdown btn if hidden list is empty
-    if (breaks.length < 1) {
-      $btn.addClass('hidden');
-      $btn.removeClass('close');
-      $hlinks.addClass('hidden');
-    }
+  if ($item.length === 0) {
+    return;
   }
 
-  // Keep counter updated
-  $btn.attr("count", breaks.length);
 
-  // update masthead height and the body/sidebar top padding
-  var mastheadHeight = $('.masthead').height();
-  $('body').css('padding-top', mastheadHeight + 'px');
-  if ($(".author__urls-wrapper button").is(":visible")) {
-    $(".sidebar").css("padding-top", "");
+  /*
+   * If a persistent item exists at the
+   * end of the navigation, insert normal
+   * navigation links before it.
+   */
+
+  if ($vlinksPersistTail.length > 0) {
+
+    $item.insertBefore(
+      $vlinksPersistTail.first()
+    );
+
   } else {
-    $(".sidebar").css("padding-top", mastheadHeight + "px");
+
+    $item.appendTo(
+      $vlinks
+    );
+
   }
 
 }
 
-// Window listeners
 
-$(window).on('resize', function () {
-  updateNav();
-});
-screen.orientation.addEventListener("change", function () {
-  updateNav();
-});
+/* =========================================
+   RESTORE ALL NAV ITEMS
+   ========================================= */
 
-$btn.on('click', function () {
-  $hlinks.toggleClass('hidden');
-  $(this).toggleClass('close');
-});
+function restoreAllLinks() {
+
+  while (
+    $hlinks.children().length > 0
+  ) {
+
+    moveHiddenItemToVisible();
+
+  }
+
+
+  breaks = [];
+
+}
+
+
+/* =========================================
+   MOBILE NAVIGATION
+   ========================================= */
+
+function setMobileNavigation() {
+
+
+  /*
+   * When entering mobile mode, first
+   * restore the navigation to its original
+   * order.
+   */
+
+  if (!mobileMode) {
+
+    restoreAllLinks();
+
+  }
+
+
+  /*
+   * Move every normal navigation item
+   * into the hamburger menu.
+   *
+   * Items with class .persist remain
+   * visible. In this theme that means
+   * the site title stays visible.
+   */
+
+  while (
+    $vlinks.children("*:not(.persist)").length > 0
+  ) {
+
+    $vlinks
+      .children("*:not(.persist)")
+      .last()
+      .prependTo(
+        $hlinks
+      );
+
+  }
+
+
+  /*
+   * Always show hamburger button.
+   */
+
+  $btn.removeClass(
+    "hidden"
+  );
+
+
+  /*
+   * Number of items inside menu.
+   */
+
+  $btn.attr(
+    "count",
+    $hlinks.children().length
+  );
+
+
+  mobileMode = true;
+
+}
+
+
+/* =========================================
+   DESKTOP GREEDY NAVIGATION
+   ========================================= */
+
+function setDesktopNavigation() {
+
+
+  /*
+   * If coming from mobile mode,
+   * restore all links first.
+   */
+
+  if (mobileMode) {
+
+    restoreAllLinks();
+
+    mobileMode = false;
+
+  }
+
+
+  var availableSpace =
+    $btn.hasClass("hidden")
+
+      ? $nav.width()
+
+      : $nav.width() -
+        $btn.width() -
+        30;
+
+
+
+  /*
+   * ---------------------------------------
+   * Navigation is overflowing
+   * ---------------------------------------
+   */
+
+  if (
+    $vlinks.width() >
+    availableSpace
+  ) {
+
+
+    while (
+
+      $vlinks.width() >
+        availableSpace &&
+
+      $vlinks
+        .children("*:not(.persist)")
+        .length > 0
+
+    ) {
+
+
+      /*
+       * Remember width at which
+       * this item disappeared.
+       */
+
+      breaks.push(
+        $vlinks.width()
+      );
+
+
+      /*
+       * Move last normal item
+       * into dropdown.
+       */
+
+      $vlinks
+        .children("*:not(.persist)")
+        .last()
+        .prependTo(
+          $hlinks
+        );
+
+
+      /*
+       * Show hamburger.
+       */
+
+      $btn.removeClass(
+        "hidden"
+      );
+
+
+      /*
+       * Recalculate available width.
+       */
+
+      availableSpace =
+        $nav.width() -
+        $btn.width() -
+        30;
+
+    }
+
+
+  }
+
+
+  /*
+   * ---------------------------------------
+   * Space is available
+   * ---------------------------------------
+   */
+
+  else {
+
+
+    while (
+
+      breaks.length > 0 &&
+
+      availableSpace >
+        breaks[
+          breaks.length - 1
+        ]
+
+    ) {
+
+
+      /*
+       * Move first hidden item
+       * back into visible navigation.
+       */
+
+      moveHiddenItemToVisible();
+
+
+      breaks.pop();
+
+
+      /*
+       * Recalculate space because
+       * visible navigation changed.
+       */
+
+      availableSpace =
+        $nav.width() -
+        $btn.width() -
+        30;
+
+    }
+
+
+
+    /*
+     * Hide hamburger if nothing
+     * remains inside the dropdown.
+     */
+
+    if (
+      $hlinks.children().length === 0
+    ) {
+
+      $btn.addClass(
+        "hidden"
+      );
+
+      $btn.removeClass(
+        "close"
+      );
+
+      $hlinks.addClass(
+        "hidden"
+      );
+
+    }
+
+  }
+
+
+  $btn.attr(
+    "count",
+    $hlinks.children().length
+  );
+
+}
+
+
+/* =========================================
+   UPDATE NAVIGATION
+   ========================================= */
+
+function updateNav() {
+
+
+  var isMobile =
+    window.innerWidth <=
+    mobileBreakpoint;
+
+
+
+  if (isMobile) {
+
+    setMobileNavigation();
+
+  } else {
+
+    setDesktopNavigation();
+
+  }
+
+
+
+  /* =======================================
+     UPDATE PAGE POSITION
+     ======================================= */
+
+  var mastheadHeight =
+    $(".masthead").height();
+
+
+  $("body").css(
+    "padding-top",
+    mastheadHeight + "px"
+  );
+
+
+  if (
+    $(".author__urls-wrapper button")
+      .is(":visible")
+  ) {
+
+    $(".sidebar").css(
+      "padding-top",
+      ""
+    );
+
+  } else {
+
+    $(".sidebar").css(
+      "padding-top",
+      mastheadHeight + "px"
+    );
+
+  }
+
+}
+
+
+/* =========================================
+   WINDOW RESIZE
+   ========================================= */
+
+$(window).on(
+  "resize",
+  function () {
+
+    updateNav();
+
+  }
+);
+
+
+/* =========================================
+   ORIENTATION CHANGE
+   ========================================= */
+
+if (
+  screen.orientation &&
+  screen.orientation.addEventListener
+) {
+
+  screen.orientation.addEventListener(
+    "change",
+    function () {
+
+      updateNav();
+
+    }
+  );
+
+}
+
+
+/* =========================================
+   HAMBURGER BUTTON
+   ========================================= */
+
+$btn.on(
+  "click",
+  function () {
+
+    $hlinks.toggleClass(
+      "hidden"
+    );
+
+    $(this).toggleClass(
+      "close"
+    );
+
+  }
+);
+
+
+/* =========================================
+   INITIALIZE
+   ========================================= */
 
 updateNav();
